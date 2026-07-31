bf651b97d3cf9d3ecc938cdaffb2bc6c96a04d8e
yuzarsif3016 <yusufpathan3016@gmail.com>
2026-07-31 19:29:37 +0530

    h2 database added

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -39,10 +39,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        Optional<Category> optinalCategory =  categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
+        Optional<Category> optionalCategory =  categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst();
 
-        if(optinalCategory.isPresent()){
-            Category existingCategory = optinalCategory.get();
+        if(optionalCategory.isPresent()){
+            Category existingCategory = optionalCategory.get();
             existingCategory.setCategoryName(category.getCategoryName());
             return existingCategory;
         }else{
